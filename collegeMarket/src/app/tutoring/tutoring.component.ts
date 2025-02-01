525661db8d538b03b06cf2957abd5cf1d0a23400
hmcintos <hm5981472@gmail.com>
2025-02-01 00:12:55 -0500

    Added the tutoring and carreehub components

---
+++ collegeMarket/src/app/tutoring/tutoring.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutoring',
+  imports: [],
+  templateUrl: './tutoring.component.html',
+  styleUrl: './tutoring.component.css'
+})
+export class TutoringComponent {
+
+}
