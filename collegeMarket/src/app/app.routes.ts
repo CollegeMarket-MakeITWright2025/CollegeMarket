7fc6ca11a7e64ac6e0a4bf146ff4094aac7f5974
hmcintos <hm5981472@gmail.com>
2025-02-01 08:16:53 -0500

    Updated directory css and the routes

--- collegeMarket/src/app/app.routes.ts
+++ collegeMarket/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { MarketComponent } from './Market/market.component';
 import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
