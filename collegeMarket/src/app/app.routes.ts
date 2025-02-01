6a17805b8d6b09c40fe27791b3656016d604b8cd
hmcintos <hm5981472@gmail.com>
2025-01-31 23:45:39 -0500

    connected home and market in the routes

--- collegeMarket/src/app/app.routes.ts
+++ collegeMarket/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MarketComponent } from './Market/market.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: AppComponent},
+    {
+    path: 'market',
+    component: MarketComponent,
+    }
+];
