21b3802f1d327ba3e768409bdef769991c443cf7
hmcintos <hm5981472@gmail.com>
2025-01-31 23:50:30 -0500

    Styled up home and market so that the nav bar showed in them both

--- collegeMarket/src/app/app.routes.ts
+++ collegeMarket/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { MarketComponent } from './Market/market.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
     path: '',
-    component: AppComponent},
+    title: "homepage",
+    component: HomeComponent},
     {
     path: 'market',
+    title: "Student Market",
     component: MarketComponent,
     }
 ];
